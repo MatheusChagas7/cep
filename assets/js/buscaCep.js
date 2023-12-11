f5b62ce851ee6faec4045a34dcc60d9acfcaf9a2
MatheusChagas7 <matheusch7@gmail.com>
2023-12-11 14:58:08 -0300

    consulta cep, atualizando html, css e js

--- assets/js/buscaCep.js
+++ assets/js/buscaCep.js
@@ -1,5 +1,7 @@
 function buscaCep(){
+
     const inputCep = document.getElementById('inputCep');
+    const container = document.getElementById('principal');
     const retorno = document.getElementById('retorno');
 
     const cep = inputCep.value.replace(/\D/g, ''); // Remover caracteres não númericos
@@ -18,14 +20,31 @@ function buscaCep(){
                 alert('CEP não encontrado. Tente novamente!');
             }else{
                 retorno.classList.remove('hidden');
+                retorno.classList.add('dados');
+                container.classList.add('hidden');
+                container.classList.remove('container');
                 document.getElementById('cep').textContent = data.cep;
                 document.getElementById('logradouro').textContent = data.logradouro;
                 document.getElementById('bairro').textContent = data.bairro;
                 document.getElementById('cidade').textContent = `${data.localidade}/${data.uf}`
+                inputCep.value = ''
             }
         })
         .catch(error => {
             console.error('Errou ao buscar CEP', error);
             alert('Ocorreu um erro ao consultar o CEP. Tente novamente!')
         });
+
+}
+
+function voltar(){
+
+    const container = document.getElementById('principal');
+    container.classList.remove('hidden');
+    container.classList.add('container');
+
+    const retorno = document.getElementById('retorno');
+    
+    retorno.classList.remove('dados');
+    retorno.classList.add('hidden');
 }
